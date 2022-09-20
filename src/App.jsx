4cd1bb22e3b508f87b50712b9a88db74c4fb2324
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-20 13:51:14 -0500

    FullstackOpen part8 section b : handling mutation errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
+import Notify from './components/Notify';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
