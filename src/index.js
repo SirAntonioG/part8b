b814c1fc47e5d3c91f1bb25b480a6399f4cfc79a
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-22 18:30:42 -0500

    Fullstack part8d adding a token to a header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { setContext } from '@apollo/client/link/context';
 
 import {
   ApolloClient,
@@ -10,11 +11,21 @@ import {
   gql,
 } from '@apollo/client';
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('phonenumbers-user-token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
+  link: authLink.concat(httpLink),
 });
 
 const query = gql`
