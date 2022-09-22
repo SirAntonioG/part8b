11e846c8e63e14743e25e3d8323fae7016faacc8
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-22 18:26:42 -0500

    Fullstack part8d user login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 import Notify from './components/Notify';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
@@ -7,9 +8,11 @@ import PhoneForm from './components/PhoneForm';
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>loading...</div>;
@@ -22,9 +25,25 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm />
